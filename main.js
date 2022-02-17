cc40e6f72edeb365fb5126ae37b7d8c3cd43f947
Joseph Maifeld <josephmaifeld@gmail.com>
2022-02-17 11:17:13 -0600

    functions parameters

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (startingNumber) => {
+    const quartered = startingNumber / 4
+
+    return quartered
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(256)
+
+// Log the return value
+console.log(result)
